238eb00b566ccf60e000ba44557346e988c15653
Kody Maus <auto.mausx@gmail.com>
2020-09-19 18:00:01 -0700

    added notes on how to use random number API and wheel

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -18,3 +18,19 @@ export default class App extends Component {
         )
     }
 };
+
+
+/*
+Notes on how to get as random of a number (list item) as possible:
+
+Use an API call to random.org with a range from 1 to the lists length
+
+Get name from list whos index matches the random number from the API call
+
+Use a prize wheel npm (or create your own) to insert random API number
+  and insert it as the winning number
+  - possible solutions:
+      https://www.npmjs.com/package/timmattison-winwheel
+      http://dougtesting.net/winwheel/docs/tut14_setting_the_prize
+
+*/
